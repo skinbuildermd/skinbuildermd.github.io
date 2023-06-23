5b42e6b30cc0c6939e8257c2625e7d18abbef22c
Max Orlov <68651897+ZeusBlockTuber@users.noreply.github.com>
2023-06-23 15:07:14 +0300

    up

--- js/compile-skin.js
+++ js/compile-skin.js
@@ -1,55 +1,53 @@
 function compileSkin() {
-    var selectedFile = document.getElementById('inputImage').files[0];
-    var clothesElement = document.getElementById('verh');
-    var clothesImage = clothesElement.querySelector('img');
-    var clothesSrc = clothesImage.getAttribute('src');
-    var nogiElement = document.getElementById('nogi');
-    var nogiImage = nogiElement.querySelector('img');
-    var nogiSrc = nogiImage.getAttribute('src');
-  
-    var canvas = document.createElement('canvas');
-    var ctx = canvas.getContext('2d');
-    var img = new Image();
-  
-    img.onload = function () {
-      canvas.width = img.width;
-      canvas.height = img.height;
-      ctx.drawImage(img, 0, 0);
-  
-      var installedImage = document.querySelector('#verh.installed img');
-      if (installedImage) {
-        var imageSrc = installedImage.src.replace('models', 'skins');
-        var fileName = imageSrc.split('/').pop();
-        clothesSrc = './img/skins/verh/' + fileName;
-      } else {
-        clothesSrc = clothesSrc.replace('models', 'skins');
-      }
-  
-      var nogiImg = new Image();
-      nogiImg.onload = function () {
-        ctx.drawImage(nogiImg, 0, 0, img.width, img.height);
-  
-        var clothesImg = new Image();
-        clothesImg.onload = function () {
-          ctx.drawImage(clothesImg, 0, 0, img.width, img.height);
-  
-          var skinImage = document.getElementById('skin-sel');
-          var skinSrc = skinImage.getAttribute('src');
-          var skinImg = new Image();
-          skinImg.onload = function () {
-            ctx.drawImage(skinImg, 0, 0, img.width, img.height);
-  
-            var downloadLink = document.createElement('a');
-            downloadLink.href = canvas.toDataURL();
-            downloadLink.download = 'compiled_skin.png';
-            downloadLink.click();
-          };
-          skinImg.src = skinSrc;
+  var selectedFile = document.getElementById('inputImage').files[0];
+  var clothesElement = document.getElementById('verh');
+  var clothesImage = clothesElement.querySelector('img');
+  var clothesSrc = clothesImage.getAttribute('src');
+  var nogiElement = document.getElementById('nogi');
+  var nogiImage = nogiElement.querySelector('img');
+  var nogiSrc = nogiImage.getAttribute('src');
+
+  var canvas = document.createElement('canvas');
+  var ctx = canvas.getContext('2d');
+  var img = new Image();
+
+  img.onload = function () {
+    canvas.width = img.width;
+    canvas.height = img.height;
+    ctx.drawImage(img, 0, 0);
+
+    var installedImages = document.querySelectorAll('#verh.installed img');
+    installedImages.forEach(function (installedImage) {
+      var imageSrc = installedImage.src.replace('models', 'skins');
+      var fileName = imageSrc.split('/').pop();
+      clothesSrc = './img/skins/verh/' + fileName;
+    });
+
+    var nogiImg = new Image();
+    nogiImg.onload = function () {
+      ctx.drawImage(nogiImg, 0, 0, img.width, img.height);
+
+      var clothesImg = new Image();
+      clothesImg.onload = function () {
+        ctx.drawImage(clothesImg, 0, 0, img.width, img.height);
+
+        var skinImage = document.getElementById('skin-sel');
+        var skinSrc = skinImage.getAttribute('src');
+        var skinImg = new Image();
+        skinImg.onload = function () {
+          ctx.drawImage(skinImg, 0, 0, img.width, img.height);
+
+          var downloadLink = document.createElement('a');
+          downloadLink.href = canvas.toDataURL();
+          downloadLink.download = 'compiled_skin.png';
+          downloadLink.click();
         };
-        clothesImg.src = clothesSrc.replace('models', 'skins');
+        skinImg.src = skinSrc;
       };
-      nogiImg.src = nogiSrc.replace('models', 'skins');
+      clothesImg.src = clothesSrc.replace('models', 'skins');
     };
-  
-    img.src = URL.createObjectURL(selectedFile);
-  }
+    nogiImg.src = nogiSrc.replace('models', 'skins');
+  };
+
+  img.src = URL.createObjectURL(selectedFile);
+}
